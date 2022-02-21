847c70ff87356bd5da9c5fbace47aa9a7bb83e18
SoiGitRepo <l2323170229@gmail.com>
2022-02-21 09:46:04 +0800

    first try

---
+++ lib/global_providers.dart
@@ -0,0 +1,11 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kanji_remake/services/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError();
+});
+
+final authServiceProvider = Provider<AuthService>((ref) {
+  return AuthService();
+});
